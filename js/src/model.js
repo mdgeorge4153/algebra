8ebdfb8e936a2d1d3c7218dd8229c5d11821916f
Michael George <mdgeorge@cs.cornell.edu>
2015-01-02 18:14:21 -0500

    started sketch

---
+++ js/src/model.js
@@ -0,0 +1,26 @@
+
+function(field) {
+
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+function Tan(coords) {
+  this.coords = coords;
+}
+
+Tan.prototype.contains = function (x, y) {
+  
+};
+
+Tan.prototype.draw = function () {
+};
+
+function Tangrams() {
+  this.tans = [];
+}
+
+}
+
+
