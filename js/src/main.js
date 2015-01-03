9ae4a8752b4a3431a70d7b31e10fefc419405536
Michael George <mdgeorge@cs.cornell.edu>
2015-01-02 20:40:45 -0500

    got algebra to compile

--- js/src/main.js
+++ js/src/main.js
@@ -1,11 +1,12 @@
 requirejs.config({
+  baseUrl: "src",
   paths: {
     "lib": "../lib"
   }
 });
 
-require(["html", "lib/domReady!"],
-function( html,   doc ) {
+require(["html", "lib/domReady!", "algebra"],
+function( html,   doc,             algebra ) {
 
 doc.body.appendChild(html.example());
 
