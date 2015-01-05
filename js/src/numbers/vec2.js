1d758e8f53fbb81016ec0049a2fb07b4c525f344
Michael George <mdgeorge@cs.cornell.edu>
2015-01-05 17:31:04 -0500

    vector implementation

---
+++ js/src/numbers/vec2.js
@@ -0,0 +1,43 @@
+define(["algebra"],
+function(algebra) {
+
+function Vector(x,y) {
+  this.x = x;
+  this.y = y;
+}
+
+function Vec2(field) {
+
+  var V = {};
+
+  V.equals = function equals(v1, v2) {
+    return field.equals(v1.x, v2.x) && field.equals(v1.y, v2.y);
+  };
+
+  V.zero = new Vector(field.zero, field.zero);
+  Object.freeze(V.zero);
+
+  V.plus = function plus(v1, v2) {
+    return new Vector(field.plus(v1.x, v2.x), field.plus(v1.y, v2.y));
+  };
+
+  V.neg = function neg (v) {
+    return new Vector(field.neg(v.x), field.neg(v.y));
+  };
+
+  V.smult = function smult(s, v) {
+    return new Vector(field.times(s, v.x), field.times(s, v.y));
+  };
+
+  if (Interface.isImplementation(field, algebra.Field))
+    algebra.VectorSpace.instantiate(V);
+  else
+    algebra.Module.instantiate(V);
+
+  Object.freeze(V);
+  return V;
+}
+
+return Vec2;
+
+});
