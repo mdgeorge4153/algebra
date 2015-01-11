421efe844bbe52dbd6067ad5c7ff698437512419
Michael George <mdgeorge@cs.cornell.edu>
2015-01-11 11:13:24 -0500

    started implementing rotation (incomplete)

--- js/src/controller.js
+++ js/src/controller.js
@@ -1,13 +1,21 @@
-define(["lib/domReady!"],
-function(doc) {
-return function Controller(model, view) {
+define(["lib/domReady!", "numbers/mat2"],
+function(doc,             Mat2) {
+return function Controller(model, view, cos, sin) {
 
 /** selection is either null (in which case we are not dragging) or it is a
     collection of vectors relative to the mouse defining the
     currently-being-dragged tan. The tan is given by model.hover.  */
 var selection = null;
-
+var F = model.F;
 var V = model.V;
+var M = Mat2(F);
+
+if (cos === undefined || sin === undefined) {
+  cos = F.zero;
+  sin = F.one;
+}
+
+var rotation = M.rotation(cos, sin);
 
 function onMouseMove(e) {
   var pos = view.getEventCoords(e);
@@ -43,11 +51,23 @@ function onMouseUp(e) {
   selection = null;
 }
 
+function onWheel(e) {
+  if (model.hover === -1)
+    return;
+
+  var tan    = model.tans[model.hover].coords;
+  var center = V.sdiv(tan.reduce(V.plus.bind(V)), F.fromInt(tan.length));
+
+  var pos    = view.getEventCoords(e);
+}
+
+
 view.canvas.addEventListener("mousemove",  onMouseMove);
 view.canvas.addEventListener("mouseenter", onMouseMove);
 view.canvas.addEventListener("mouseout",   onMouseOut);
 view.canvas.addEventListener("mousedown",  onMouseDown);
 view.canvas.addEventListener("mouseup",    onMouseUp);
+view.canvas.addEventListener("wheel",      onWheel);
 
 };
 
