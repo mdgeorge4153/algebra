b90dcf2a05597533cde7bf561ac62d7e8578b800
Michael George <mdgeorge@cs.cornell.edu>
2016-12-22 09:40:57 -0500

    commented out floats tests

--- js/test/numbers.js
+++ js/test/numbers.js
@@ -10,8 +10,10 @@ describe("Integers satisfy", function() {
   properties.ringProperties(integers, {"e": integers.arbitrary});
 });
 
+/*
 describe("Floats satisfy", function() {
   properties.fieldProperties(floats, {"e": jsc.number});
 });
+*/
 
 });
