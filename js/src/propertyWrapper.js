db2f84cb6ff1514a5e718ec8d3288eb3546c2389
Michael George <mdgeorge@cs.cornell.edu>
2016-12-27 16:08:18 -0500

    good failure messages

--- js/src/propertyWrapper.js
+++ js/src/propertyWrapper.js
@@ -1,13 +1,44 @@
 define(["lib/jsverify"],
 function(jsc) {
 
+beforeEach(function() {
+
+  jasmine.addMatchers({
+    toHold: function() {
+      return {
+        compare: function(actual) {
+	  /* global window */
+	  var quiet = window && !(/verbose=true/).test(window.location.search);
+
+	  var r = jsc.check(actual, { quiet: quiet });
+
+	  var pass = r === true;
+	  var message = "";
+
+	  if (pass) {
+	    message = "Expected property not to hold.";
+	  } else {
+	    message = "Expected property to hold. Counterexample found: " + r.counterexamplestr;
+	  }
+
+	  return {
+	    pass: pass,
+	    message: message,
+	  };
+        }
+      };
+    }
+  });
+
+});
+
 return function property(name) {
   var args = Array.prototype.slice.call(arguments, 1);
   if (args.length == 1)
     args.splice(0,0,jsc.unit);
   var prop = jsc.forall.apply(null,args);
   it(name, function() {
-    expect(jsc.check(prop)).toBe(true);
+    expect(prop).toHold();
   });
 };
 
