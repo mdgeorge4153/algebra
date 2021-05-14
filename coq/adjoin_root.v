d1f8978db19b9252d3c085df6fd85e0499f85679
Michael George <mdgeorge@cs.cornell.edu>
2021-05-14 17:43:31 -0400

    progress on some janky proofs for adjoin ordering

--- coq/adjoin_root.v
+++ coq/adjoin_root.v
@@ -120,7 +120,7 @@ End decfield.
 (******************************************************************************)
 Section orders.
 
-Context `{Ring OR} `{!SemiRingOrder Rle} `{n : OR} `{n_nonneg : 0 ≤ n}.
+Context `{Ring OR} `{!FullPseudoSemiRingOrder Rle Rlt} `{!TotalOrder (≤)} `{n : OR} {n_nonneg : 0 ≤ n}.
 
 Add Ring OR : (stdlib_ring_theory OR).
 Add Ring AR : (stdlib_ring_theory (AdjoinRoot OR n)).
@@ -133,11 +133,9 @@ Definition nonneg (x : AdjoinRoot OR n) : Prop :=
   *)
   let a := ones x in
   let b := roots x in
-  (0 ≤ a /\ 0 ≤ b)
+  (zero ≤ a /\ b * b ≤ n * a * a)
   \/
-  (0 ≤ a /\ b * b ≤ n * a * a)
-  \/
-  (0 ≤ b /\ n * a * a ≤ b * b).
+  (zero ≤ b /\ n * a * a ≤ b * b).
 
 Global Instance Adjoin_le : Le (AdjoinRoot OR n) :=
   λ x y,nonneg (y - x).
@@ -151,8 +149,119 @@ Qed.
 
 Instance: Reflexive Adjoin_le.
 Proof.
-repeat red; intro x; left; split; ring_simplify (x - x); firstorder.
+repeat red; intro x; simpl; left; split;
+match goal with
+  | [ |- ?lhs ≤ ?rhs ] => ring_simplify lhs; ring_simplify rhs; firstorder
+end.
 Qed.
+
+Lemma ineq_sum: forall (a b c d : OR), a ≤ b -> c ≤ d -> a + c ≤ b + d.
+Proof.
+intros; transitivity (a + d);
+  [ apply (order_preserving (a +))
+  | apply (order_preserving (+ d))
+  ]; auto.
+Qed.
+
+Lemma nonneg_OR_sum: forall (a b : OR), 0 ≤ a -> 0 ≤ b -> 0 ≤ a + b.
+Proof. intros; setoid_replace 0 with (0 + 0) by ring; apply (ineq_sum 0 a 0 b); auto.
+Qed.
+
+Lemma ineq_prod: forall (a b c d: OR), 0 ≤ c ≤ d -> 0 ≤ a ≤ b -> c * a ≤ d * b.
+Proof. intros.
+assert (c * a ≤ c * b).
+  apply flip_nonneg_minus. setoid_replace (c*b - c*a) with (c * (b - a)) by ring.  apply nonneg_mult_compat. firstorder. apply flip_nonneg_minus. firstorder.
+assert (c * b ≤ d * b).
+  apply flip_nonneg_minus. setoid_replace (d*b - c*b) with (b * (d - c)) by ring.  apply nonneg_mult_compat. red. transitivity a; firstorder. apply flip_nonneg_minus. firstorder.
+transitivity (c * b); auto.
+Qed.
+
+Context `{!ZeroProduct OR}.
+
+(* TODO: this is absolutely terrible *)
+Lemma eq_sqrt: forall (a b: OR), 0 ≤ a -> 0 ≤ b -> a * a = b * b -> a = b.
+Proof. intros.
+assert ((a + b) * (a - b) = 0).
+  ring_simplify. rewrite H2. ring_simplify. auto.
+assert ((a + b) = 0 \/ (a - b) = 0).
+  apply zero_product. auto.
+destruct H4.
+  assert (b = 0).
+    apply (antisymmetry (≤)); auto. rewrite <- H4. setoid_replace b with (0 + b) at 1 by ring.  apply ineq_sum; auto.
+  rewrite H5 in H4. ring_simplify in H4. rewrite H4. auto.
+
+  setoid_replace b with (b + 0) by ring. rewrite <- H4. ring.
+Qed.
+
+Lemma ineq_sqrt: forall (a b: OR), 0 ≤ b -> a * a ≤ b * b -> a ≤ b.
+Proof. intros. destruct (total (≤) a b). auto.
+ assert (b * b ≤ a * a). apply ineq_prod; firstorder.
+assert (a * a = b * b). apply (antisymmetry (≤) (a * a) (b * b)); firstorder.
+assert (a = b). apply eq_sqrt; auto. rewrite H5. reflexivity.
+Qed.
+
+Lemma nonneg_square: forall a : OR, 0 ≤ a * a.
+Proof.
+intros; destruct (total (≤) 0 a) as [pos | neg];
+match goal with
+  | [ H : ?a ≤ 0 |- _ ] =>
+      setoid_replace (a * a) with ((-a) * (-a)) by ring;
+      apply (flip_nonpos_negate a) in H
+  | [ |- _ ] => idtac
+end;
+apply nonneg_mult_compat; auto.
+Qed.
+
+Lemma nonneg_sum: forall x y, nonneg x -> nonneg y -> nonneg (x + y).
+(* paper proof:
+ - case 1: 0 ≤ ax and bx * bx ≤ n * ax * ax, and 0 ≤ ay and by ≤ n * ay * ay
+
+x + y = ax + ay + (bx + by)√n.
+  0 ≤ ax + ay
+  wts (bx + by)^2 ≤ n (ax + ay)^2
+  i.e. bx^2 + 2bxby + by^2 ≤ nax^2 + nay^2 + 2naxay.
+  This reduces to 2 bx by ≤ 2n ax ay by the given inequalities
+  We have 0 ≤ bx * bx ≤ n * ax * ax and 0 ≤ by * by ≤ n * ay * ay.
+  Multiplying these gives
+  bx * by * bx * by ≤ n * n * ax * ay * ax * ay
+  taking the square root of this equation yields:
+  bx * by ≤ n * ax * ay
+
+
+  (2n ax ay ≤ 2bx by)?
+
+
+*)
+repeat red. unfold nonneg. intros a b nonneg_a nonneg_b. destruct nonneg_a; destruct nonneg_b.
+(* case 1/4 *)
+simpl. simpl. left. split.
+  apply nonneg_OR_sum; firstorder.
+  ring_simplify.  apply ineq_sum.
+    apply ineq_sum. firstorder. apply ineq_sqrt.
+repeat match goal with
+  | [ |- 0 ≤ ?x * ?y ] => apply nonneg_mult_compat; red
+  | [ |- 0 ≤ Aplus Aone Aone ] => apply le_0_2
+end; firstorder.
+setoid_replace (2 * roots a * roots b * (2 * roots a * roots b)) with     (4 * (roots a * roots a) * (roots b * roots b)) by ring.
+setoid_replace (2 * n * ones a * ones b * (2 * n * ones a * ones b)) with (4 * (n * ones a  * ones a)  * (n * ones b * ones b)) by ring.
+  apply ineq_prod.
+    split.
+      apply nonneg_mult_compat.
+        apply le_0_4.
+        apply (nonneg_square _).
+      apply ineq_prod.
+        split.
+          apply le_0_4.
+          reflexivity.
+        split.
+          apply (nonneg_square _).
+          intuition.
+      split.
+        apply (nonneg_square _).
+        destruct H1.
+        intuition.
+    intuition.
+
 
 Instance: Transitive Adjoin_le.
 Proof.
